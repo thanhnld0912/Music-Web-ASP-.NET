6f6b1f6957dce691f21abeec97ab434fde931dae
thanhnld0912 <joshlemuel241291@gmail.com>
2025-03-11 14:20:42 +0700

    modified assemblyinfo.cs

--- MusicWeb/obj/Debug/net8.0/MusicWeb.AssemblyInfo.cs
+++ MusicWeb/obj/Debug/net8.0/MusicWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a840ccf9ffdeb3bf90e7f8854cbc2795ca81e480")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
